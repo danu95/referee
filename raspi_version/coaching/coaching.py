b2017e46f6fb53bed27f8658e8a63b2c409c0930
vonkaenel dani <vonkaeneldanielgit@proton.me>
2026-03-14 17:58:46 +0100

    -

--- raspi_version/coaching/coaching.py
+++ raspi_version/coaching/coaching.py
@@ -176,8 +176,9 @@ def select_SR():
     # This clicks the link that goes to inspection results
     # It corresponds to 'SR Coaching Ergebnisse' in your HTML
     driver.wait_for_element_clickable('a[href="/schiedsrichter/inspektionsergebnisse"]')
-
+    print('wait_click_ok')
     driver.wait_for_element_present('a[href="/schiedsrichter/inspektionsergebnisse"]')
+    print('wait_element_present')
     driver.click('a[href="/schiedsrichter/inspektionsergebnisse"]')
     
     
